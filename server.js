8c2fc700d72c098e082287a2d08d7fc828797dcf
Balaji S <balajiyadhav0@gmail.com>
2017-03-11 18:20:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,45 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne = {
+    title: 'Article One | balaji',
+    date: 'March 11,2016',
+    content: `<p>This is my first article i like to travel</p>
+        <p>I like coding</p>`
+};
+function createTemplate(data)
+{
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = 
+    `<html>
+        <head><title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a><hr>
+                
+            </div>
+            <h3>${title}</h3>
+            <div>${date}</div>
+            <div>${content}
+            </div>
+        </div>
+    </body>
+    </html>`
+    return htmlTemplate
+    }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req, res) {
