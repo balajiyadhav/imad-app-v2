bcf7341f1a291417dc39a941b0c7b9257626b367
Balaji S <balajiyadhav0@gmail.com>
2017-03-12 00:33:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 //change the text of main
-var element=document.getElementById("main-text");
-element.innerHtml="New Value";
-console.log("new loaded");
+var element=document.getElementById('main-text');
+element.innerHtml='New Value';
+//console.log("${element}")
+console.log('new loaded');
