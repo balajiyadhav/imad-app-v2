31f51b70668f209bde637213b159e06e4a54d26f
Balaji S <balajiyadhav0@gmail.com>
2017-03-12 02:40:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("counter");
-var counter = 0;
-button.onclick = funtion(){
-    var span = document.getElementById("count");
-    counter = counter + 1;
-    span.innerHtml = counter.toString();
-};
