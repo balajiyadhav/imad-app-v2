837b14f6fb629f93da93233f77cd8db031a96d1a
Balaji S <balajiyadhav0@gmail.com>
2017-03-12 00:47:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,11 @@ element.innerHtml='New Value';
 console.log('new loaded');
 var image=document.getElementById('madi');
 
-var marginLeft = 0
+var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 5
-    image.style.marginLeft = marginLeft + 'px'
+    marginLeft = marginLeft + 5;
+    image.style.marginLeft = marginLeft + 'px';
 }
 image.onclick = function(){
-    var interval = setInterval(moveRight,100)
+    var interval = setInterval(moveRight,100);
 };
